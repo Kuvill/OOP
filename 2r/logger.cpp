92be72d8f64d77fcab7059280dc0dab3d7ad5eeb
Kuvill <lenyavr@gmail.com>
2025-06-11 22:19:48 +0200

    improved 2 laba

--- 2r/logger.cpp
+++ 2r/logger.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
 #include <vector>
 #include <string>
+#include <cassert>
 
 enum class Color {
     Red,
@@ -24,20 +26,25 @@ static constexpr const char* getColorCode(Color color) {
 class Printer {
 private:
     std::vector<std::string> _alphabet;
+    int height, _power;
     Color _color;
 
 public:
-    Printer(Color color = Color::Default ) : _color(color) {
-        std::ifstream file("font.txt");
-		std::string line;
+    Printer( std::string&& fileName, Color color ) : _color(color) {
+        std::ifstream file( fileName );
+        file >> _power >> height;
 
+        if( !file.good() )
+            throw std::runtime_error( "Wrong font file format!\nFirst line have to contain: power of alphabet, weight of an letter");
+
+		std::string line;
 		while (std::getline(file, line)) {
 			_alphabet.emplace_back( std::move(line) );
 		}
     }
 
-	// not optimal way
-    Printer(Color color, char symbol) : Printer(color) {
+
+    Printer( std::string&& fileName, Color color, char symbol) : Printer(std::move(fileName), color) {
 		for (auto& row : _alphabet) {
 			for (char& c : row) {
 				if (c != ' ') {
@@ -47,33 +54,37 @@ public:
         }
     }
 
+    Printer( std::string&& fileName = "font.txt" ) : Printer( std::move(fileName), Color::Default ) {}
+
     void print(const std::string& text) {
         std::cout << getColorCode(_color);
 
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < height; i++) {
             for (char c : text) {
                 int charIndex = c - 'A';
-                if (charIndex >= 0 && charIndex < 26) {
-                    std::cout << _alphabet[i + charIndex * 6];
+
+                if (charIndex >= 0 && charIndex < _power) {
+                    std::cout << _alphabet[i + charIndex * height];
                 }
+
             }
             std::cout << std::endl;
         }
         std::cout << "\033[0m";
     }
 
-	static void Print(const std::string& text, Color color = Color::Default, char symbol = 0) {
-		Printer printer(color, symbol);
+	static void Print(std::string&& fileName, const std::string& text, Color color = Color::Default, char symbol = 0) {
+		Printer printer(std::move(fileName), color, symbol);
 		printer.print(text);
 	}
 };
 
 int main() {
-	Printer::Print("HELLO", Color::Red, '*');
+	Printer::Print( "font.txt", "HELLO", Color::Red, '*');
 
 	std::cout << '\n';
 
-    Printer myPrinter(Color::Red);
-    myPrinter.print("WORLD");
+    Printer myPrinter("small_font.txt", Color::Red);
+    myPrinter.print("ABC");
     return 0;
 }
